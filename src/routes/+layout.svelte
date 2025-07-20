3707f8c2034ad6d1873537d190dbe469cde67eeb
andrebellu <bellu.andrea@outlook.it>
2025-07-20 16:35:34 +0200

    minor changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,6 @@
   import Navbar from "$lib/components/Navbar.svelte";
 </script>
 
-<svelte:head>
-  <link rel="preload" as="image" href="/carousel1.webp" fetchpriority="high" />
-</svelte:head>
-
 <div class="min-h-screen">
   <Navbar />
   <main class:pt-24={$page.url.pathname !== "/"}>
