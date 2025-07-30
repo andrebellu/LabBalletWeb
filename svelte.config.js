2ce61cff4d25534c59d5c6263870e84517a1aa13
andrebellu <bellu.andrea@outlook.it>
2025-07-30 22:40:53 +0200

    added images catch error

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,14 @@ import adapter from '@sveltejs/adapter-cloudflare';
 
 export default {
 	kit: {
+		prerender: {
+			handleHttpError: ({ path, status }) => {
+				if (path.startsWith('/images/') && status === 404) {
+					return;
+				}
+				throw new Error(`Unexpected ${status} on ${path}`);
+			}
+		},
 		adapter: adapter({
 			// See below for an explanation of these options
 			config: undefined,
